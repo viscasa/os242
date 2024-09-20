8fb33b3f82dba30b61c011a5f7ca1b3780940b14
viscasa <158464583+viscasa@users.noreply.github.com>
2024-09-20 20:52:04 +0700

    Finish Weekly Assignment 03

--- test/WEEK02.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Read input file
-input_file="inputSCRAP.txt"
-output_file="outputSCRAP.txt"
-
-# Extract student IDs (NPMs) and save them to the output file
-grep -oE '[0-9]{10}' "$input_file" | sort > "$output_file"
